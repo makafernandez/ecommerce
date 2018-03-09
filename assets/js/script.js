661a8bf5ff6d2368b550f435196d89d5fa4c376a
missmakita <mfernanlav@gmail.com>
2018-03-09 09:58:41 -0300

    fix routing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,24 @@
   var app = $.sammy('#main', function() {
     // VISTA HOME
     this.get('#/', function(context) {
-      console.log(this);
       $.ajax({
         url: 'https://api.mercadolibre.com/sites/MLC/search?',
         data: {
           q: 'hogar',
+          condition: 'new',
           limit: '48'
         },
         dataType: 'json',
         
         success: function(response) {
           context.log('the page was loaded', response);
-          let items = response.results;
+          app.items = response.results;
           // Grab the template script:
           const source = $('#home').html();
           // Compile the template:
           const template = Handlebars.compile(source);
           // Pass our data to the template:
-          let html = template(items);
+          let html = template(app.items);
           // Add the compiled html to the page:
           $('#main').html(html);
         }
@@ -27,19 +27,19 @@
     });
 
     // VISTA CATEGORIA
-    this.get('#/category', function(context) {
-      console.log(this);
+    this.get('#/category/:id', function(context) {
+      app.item = this.params['id'];
       $.ajax({
         url: 'https://api.mercadolibre.com/sites/MLC/search?',
         data: {
-          q: 'living',
+          q: `${app.item}`,
           limit: '48'
         },
         dataType: 'json',
         
         success: function(response) {
           context.log('the page was loaded', response);
-          let items = response.results;
+          let items = response;
           // Grab the template script:
           const source = $('#category').html();
           // Compile the template:
@@ -53,122 +53,61 @@
     });
 
     // VISTA ITEM
-    /*
     this.get('#/item/:id', function(context) {
+      app.item = this.params['id'];
       $.ajax({
-        url: 'https://api.mercadolibre.com/items',
-        id: this.id,
+        url: `https://api.mercadolibre.com/items/${app.item}`,
         dataType: 'json',
 
         success: function(response) {
           context.log('the page was loaded', response);
-          let item = response;
-
+          app.item = response;
           // Grab the template script:
           const source = $('#item').html();
           // Compile the template:
           const template = Handlebars.compile(source);
           // Pass our data to the template:
-          let html = template(item);
+          let html = template(app.item);
           // Add the compiled html to the page:
           $('#main').html(html);
         }
       });
-    }); */
-
-    $(function() {
-      app.run('#/');
     });
 
-    // BUSCADOR 
+    // VISTA RESULTADOS DE BÚSQUEDA
     let keyword = '';
     $('#searchBtn').click(function() {
       keyword = $('#searchText').val();
       console.log(keyword);
-      // Grab the template script:
-      const source = $('#search').html();
-      // Compile the template:
-      const template = Handlebars.compile(source);
+      location.hash = '/search';
+    });  
+    this.get('#/search', function(context) {
+      app.item = keyword;
+      $.ajax({
+        url: 'https://api.mercadolibre.com/sites/MLC/search?',
+        data: {
+          q: app.item,
+          condition: 'new',
+          limit: '48'
+        },
+        dataType: 'json',
 
-      // Define our data object
-      let context = '';
-      $.get(`https://api.mercadolibre.com/sites/MLC/search?q=${keyword}`, function(data) {
-        context = data.results;
-        console.log(context);
-
-        // Pass our data to the template:
-        let html = template(context);
-
-        // Add the compiled html to the page:
-        $('#main').html(html);
-
-        // Manually trigger a hashchange to start the app.
-        $(window).trigger('hashchange');
+        success: function(response) {
+          context.log('the page was loaded', response);
+          $('#searchForm')[0].reset();
+          app.items = response.results;
+          // Grab the template script:
+          const source = $('#search').html();
+          // Compile the template:
+          const template = Handlebars.compile(source);
+          // Pass our data to the template:
+          let html = template(app.items);
+          // Add the compiled html to the page:
+          $('#main').html(html);
+        }
       });
     });
   });
+  app.run('#/');
 })(jQuery);
 
-/*
-// PAGE HOME
-function getAllProducts() {
-  // Grab the template script:
-  const source = $('#allproducts-template').html();
-  
-  // Compile the template:
-  const template = Handlebars.compile(source);
-  // Define our data object
-  let products = '';
-  $.get('https://api.mercadolibre.com/sites/MLC/search?q=celular&limit=48', function(data) {
-    products = data.results;
-    console.log(products);
-    // Pass our data to the template:
-    let html = template(products);
-    // Add the compiled html to the page:
-    $('#main').html(html);
-    // Manually trigger a hashchange to start the app.
-    $(window).trigger('hashchange');
-  });
-}
-// SEARCH RESULTS:
-$('#searchBtn').click(function() {
-  let keyword = $('#searchText').val();
-  console.log(keyword);
-  $(function(keyword) {
-    // Grab the template script:
-    const source = $('#search-template').html();
-    // Compile the template:
-    const template = Handlebars.compile(source);
-    // Define our data object
-    let context = '';
-    $.get(`https://api.mercadolibre.com/sites/MLC/search?q=${keyword}`, function(data) {
-      context = data.results;
-      console.log(context);
-      // Pass our data to the template:
-      let html = template(context);
-      // Add the compiled html to the page:
-      $('#main').html(html);
-      // Manually trigger a hashchange to start the app.
-      $(window).trigger('hashchange');
-    });
-  });
-});
-// VISTA PRODUCTO:
-function getProductDetail(productID) {
-  // Grab the template script:
-  const source = $('#product-template').html();
-  // Compile the template:
-  const template = Handlebars.compile(source);
-  // Define our data object
-  let context = '';
-  $.get(`https://api.mercadolibre.com/items/${productID}`, function(data) {
-    context = data.results;
-    console.log(context);
-    // Pass our data to the template:
-    let html = template(context);
-    // Add the compiled html to the page:
-    $('#main').html(html);
-    // Manually trigger a hashchange to start the app.
-    $(window).trigger('hashchange');
-  });
-} */
